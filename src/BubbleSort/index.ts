c3c686664d37837a861a4e0203d2005371912a73
Tetiana Platonova <malyfko@gmail.com>
2018-07-13 18:56:57 +0200

    Update types

--- src/BubbleSort/index.ts
+++ src/BubbleSort/index.ts
@@ -1,6 +1,6 @@
 import { swap } from '../helpers';
 
-export const bubbleSort = (inputArray: number[]): number[] => {
+export const bubbleSort = (inputArray: ReadonlyArray<number>): number[] => {
   const outputArray = inputArray.slice();
   for (let i = 0; i < outputArray.length - 1; i++) {
     for (let j = 0; j < outputArray.length - i - 1; j++) {
